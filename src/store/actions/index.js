2b6adb44e5c9937dbc52b79f1fd7aecec36b5ec6
Humberto Junior <astronomi@gmail.com>
2019-11-25 16:58:06 -0300

    update files

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,21 @@
+import api from '../../services/api'
 export const ADD_DATE_CALENDAR = 'ADD_DATE_CALENDAR';
+export const SET_DATES = 'SET_DATES'
 
-export function addCalendar(date) {
-  return {
-    type: ADD_DATE_CALENDAR,
-    payload: date
-  };
+export const addCalendar = getDate => {
+  return dispatch => { 
+    api.get('/pagamentos', { ...getDate })
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }
 }
+
+export const setDates = getDate => {
+  return {
+    type: SET_DATES,
+    payload: getDate
+  }
+}
+
 
 
